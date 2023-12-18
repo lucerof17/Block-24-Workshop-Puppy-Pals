b7e84edb2164f0ea1b4d73cc91b502acf695861d
lucerof17 <linux@EVA-UNIT-02>
2023-12-18 13:09:50 -0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
+import { useState } from 'react'
 import './App.css'
+import {puppyList} from './data.js'
+
 
 function App() {
 
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log("puppyList: ", puppyList);
+
+  const setPuppies = setPuppies 
+
   return (
-    <>
-      <div>
-        change
-      </div>
-    </>
-  )
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+             return <p key={puppy.id}>{puppy.name}</p>;
+           })
+       }
+    </div>
+  );
 }
+
+
 
 export default App
 
